319199e15aa8be76b44f064dbd0a495085106be0
Juan Sebastian Terreros <96632920+sebastianterreros@users.noreply.github.com>
2025-07-24 19:03:11 -0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
+// vue.config.js
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // Configura publicPath solo para producción
+  publicPath: process.env.NODE_ENV === 'production'
+    // El nombre del repositorio es 'proyectovue', por lo tanto, la ruta base es '/proyectovue/'
+    ? '/proyectovue/'
+    : '/' // Para desarrollo local, la ruta base sigue siendo la raíz
 })
